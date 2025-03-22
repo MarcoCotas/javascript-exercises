61e22a61a2fab4e7d88bfccf727761c80a8f01f0
MarcoCotas <marco.cotas06@gmail.com>
2025-03-22 12:06:52 +0000

    Finsih the 3 exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,8 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let strArray = string.split("");
+    let reverseArray = strArray.reverse();
+    let reverseString = reverseArray.join("");
+    return reverseString
 
 };
 
