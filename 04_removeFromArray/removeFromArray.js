922f91bccdbcd9f6fe3827f8cc5dedf5cc37df1e
MarcoCotas <marco.cotas06@gmail.com>
2025-03-24 18:16:48 +0000

    Finish exercise 4 of JS

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+/* 
+1 - Create a variable to track whatś out of the array
+2 - A loop to check if anything in the array = to anythin in the variable
+3 - remove the equal values from the array
+4 - return only the new array
+
+
+*/
+
+const removeFromArray = function(array, ...args) {
+   let newArray = [];
+   for (let i = 0; i < array.length; i++) {
+       if (!args.includes(array[i])) {
+           newArray.push(array[i]);
+       }
+   }
+   return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
