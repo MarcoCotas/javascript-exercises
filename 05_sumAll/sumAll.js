379b7e29f70dac62bef03b2fd249905779463dd7
MarcoCotas <marco.cotas06@gmail.com>
2025-03-24 19:02:01 +0000

    Finish exercise 5 JS

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  if (
+    typeof min !== "number" ||
+    typeof max !== "number" ||
+    min < 0 ||
+    max < 0 ||
+    min % 1 !== 0 ||
+    max % 1 !== 0
+  ) {
+    return "ERROR";
+  }
 
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+
+  let total = 0;
+  for (let i = min; i <= max; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
