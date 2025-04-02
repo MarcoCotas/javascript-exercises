a0340bb7d78d8c227275c2f92d273358ae796453
MarcoCotas <marco.cotas06@gmail.com>
2025-04-02 20:16:10 +0100

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function () {};
-fsdfs;
-fsadfsd;
+const sumAll = function (start, end) {
+  if (
+    !Number.isInteger(start) ||
+    !Number.isInteger(end) ||
+    start < 0 ||
+    end < 0
+  ) {
+    return "ERROR";
+  }
+
+  if (start > end) [start, end] = [end, start];
+
+  let sumAll = 0;
+  for (let i = start; i <= end; i++) {
+    sumAll += i;
+  }
+  return sumAll;
+};
+
 // Do not edit belfasfasow this line
 module.exports = sumAll;
