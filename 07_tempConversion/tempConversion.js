eee5452d905e5c09a23d92b38659de3a14cfa08f
MarcoCotas <marco.cotas06@gmail.com>
2025-04-03 07:52:02 +0100

    Finish exercise7 of TempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  conversionFToC = (fahrenheit - 32) * (5 / 9);
+  let celsius = Math.round(conversionFToC * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let conversionCToF = celsius * (9 / 5) + 32;
+  let fahrenheit = Math.round(conversionCToF * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
