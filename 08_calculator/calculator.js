925893477c4d0c01e3b676cf44a197925c5097ab
MarcoCotas <marco.cotas06@gmail.com>
2025-04-03 09:24:38 +0100

    FInish exercise8 - calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sumArray = array.reduce(
+    (acumulator, currentValue) => acumulator + currentValue,
+    0
+  );
+  return sumArray;
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.reduce((acu, num) => acu * num, 1);
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num1) {
+  if (num1 === 0 || num1 === 1) {
+    return 1;
+  }
+  let factorialNum = 1;
+  for (let i = 2; i <= num1; i++) {
+    factorialNum *= i;
+  }
+
+  return factorialNum;
 };
 
 // Do not edit below this line
@@ -29,5 +41,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
